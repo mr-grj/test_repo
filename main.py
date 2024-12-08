40ed68b29663ef795cf59f8c40d229842baa4714
Alexandru Grajdeanu <grajdeanu.alex@gmail.com>
2024-12-08 12:56:30 +0200

    Update settings.json and update code

--- main.py
+++ main.py
@@ -2,4 +2,15 @@ def get_odd_numbers(numbers: tuple) -> tuple:
     return tuple(number for number in numbers if number % 2 == 1)
 
 
-print(get_odd_numbers((1, 2, 3, 4, 5, 6)))
+def get_even_numbers(numbers: tuple) -> tuple:
+    return tuple(number for number in numbers if number % 2 == 0)
+
+
+def main():
+    numbers = (1, 2, 3, 4, 5, 6)
+    print(get_odd_numbers(numbers))
+    print(get_even_numbers(numbers))
+
+
+if __name__ == "__main__":
+    main()
